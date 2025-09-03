b7d8a14663c4012bf2da2fd4ab0554a44c14075b
matheus <theusantunesm@gmail.com>
2025-09-03 10:39:10 -0300

    importando e habilitando rotas na aplicação

---
+++ projetocalculadoracp4/src/App.jsx
@@ -0,0 +1,32 @@
+import Footer from './components/Footer'
+
+
+
+
+
+
+  return (
+    // HABILITA A NAVEGAÇÃO POR ROTAS NA APLICAÇÃO
+    <BrowserRouter>
+    <Nav/>
+    <main>
+      {/* Gerencia a exibição dos componentes com base na url */}
+      <Routes>
+        {/* Rota para lidar com as urls não encontradas */}
+        <Route path="*" element={<Error/>}/>
+
+        {/* Rota para chamar a página home */}
+        <Route path="/" element={<Home/>}/>
+
+         {/* Rota para chamar a página Cliente */}
+        <Route path="/cliente" element={<Cliente/>}/>
+        
+
+      </Routes>
+    </main>
+     <Footer/>
+    </BrowserRouter>
+  )
+}
+
+export default App
