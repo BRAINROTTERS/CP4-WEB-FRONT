5424aaff8b98da0d55e56d9756e0f2c7b5b55aa8
Rafael <rafaelmrdsantos@hotmail.com>
2025-09-04 23:30:39 -0300

    Mudando o Titulo da pagina e colocando um shortcuticon

--- projetocalculadoracp4/src/components/Nav.jsx
+++ projetocalculadoracp4/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <nav className="bg-gray-900 text-gray-200 px-6 py-4 flex justify-between items-center shadow-md">
-      <h1 className="text-2xl font-bold text-blue-400">CarShop</h1>
+      <h1 className="text-2xl font-bold text-blue-400">BrainRotters DealerShip</h1>
       <ul className="flex gap-6">
          <Link to="/" className="hover:text-blue-400 transition">Home</Link>
           <Link to="/calculadora-normal" className="hover:text-blue-400 transition"> Calculadora </Link>
